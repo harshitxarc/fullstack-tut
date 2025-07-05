751a261eb125f565bd85358d1e22f2cb6e6073af
harshitxarc <harshitverma0010@gmail.com>
2025-07-05 21:40:32 +0530

    Add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import Button from './Button.jsx';
 // import ProfilePicture from './ProfilePicture.jsx';
-// import Components from './Components.jsx';
+import Components from './Components.jsx';
 // import MyComponent from './MyComponent.jsx';
 // import ColorPicker from './ColorPickerApp/ColorPicker.jsx';
 
 function App() {
   return(
   <>
+  <Components />
   </>
   );
 }
