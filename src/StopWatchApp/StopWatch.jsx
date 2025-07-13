09414ba185ceea86b0c146548dd4fcbb2e0d8306
harshitxarc <harshitverma0010@gmail.com>
2025-07-13 15:39:21 +0530

    Modify StopWatch Component

--- src/StopWatchApp/StopWatch.jsx
+++ src/StopWatchApp/StopWatch.jsx
@@ -3,23 +3,51 @@ import './StopWatch.css';
 
 function StopWatch(){
 
-    const [isRunning, setIsRunning] = useState("false");
+    const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setIsElapsedTime] = useState(0);
     const IntervalIdRef = useRef(null);
     const startTimeRef = useRef(0);
 
     useEffect(() => {
+        if(isRunning){
+            IntervalIdRef.current = setInterval(() => {
+                setIsElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
 
+        return()=>{
+            clearInterval(IntervalIdRef.current);
+        }
     }, [isRunning]);
 
-    function start(){}
+    function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+        console.log(startTimeRef.current);
+    }
 
-    function stop(){}
+    function stop(){
+        setIsRunning(false);
+    }
 
-    function reset(){}
+    function reset(){
+        setIsElapsedTime(0);
+        setIsRunning(false);
+    }
 
     function formatTime(){
-        return `00:00:00`;
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let miliSeconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2,"0");
+        minutes = String(minutes).padStart(2,"0");
+        seconds = String(seconds).padStart(2,"0");
+        miliSeconds = String(miliSeconds).padStart(2,"0");
+
+        return `${minutes}:${seconds}:${miliSeconds}`;
     }
 
 
