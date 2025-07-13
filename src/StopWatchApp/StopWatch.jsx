76321f6f4ef0905ac870181cba060bc72983c34c
harshitxarc <harshitverma0010@gmail.com>
2025-07-13 14:29:09 +0530

    Update StopWatch.jsx

--- src/StopWatchApp/StopWatch.jsx
+++ src/StopWatchApp/StopWatch.jsx
@@ -1,7 +1,37 @@
-import React, {useState, useEffect, UseRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 ;
 function StopWatch(){
 
+    const [isRunning, setIsRunning] = useState("false");
+    const [elapsedTime, setIsElapsedTime] = useState(0);
+    const IntervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+
+    }, [isRunning]);
+
+    function start(){}
+
+    function stop(){}
+
+    function reset(){}
+
+    function formatTime(){
+        return `00:00:00`;
+    }
+
+
+    return(
+        <div className='stopwatch'>
+            <div className='display'>{formatTime()}</div>
+            <div className='controls'>
+                <button className='start-button' onClick={start}>Start</button>
+                <button className='stop-button' onClick={stop}>Stop</button>
+                <button className='reset-button' onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default StopWatch;
